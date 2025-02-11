eb06d015913197ba8ce4a408126e768fb9abbf64
sandratriveno <sandra.triveno@gmail.com>
2025-02-11 08:09:20 -0500

    initial project

---
+++ src/main/java/pe/edu/idat/app_config_server/AppConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_config_server/AppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
